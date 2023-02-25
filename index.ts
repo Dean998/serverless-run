fa11a1f64bf568fb69392374b71ffeb4364e3b2d
Extra <extra@Deans-MBP.home>
2023-02-25 23:20:55 +0000

    added handler

---
+++ index.ts
@@ -0,0 +1,16 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResultV2,
+  Handler,
+} from "aws-lambda";
+import * as _ from "lodash";
+
+export const handler: Handler = async (event: APIGatewayProxyEvent) => {
+  const max = 10;
+  const val = _.random(max);
+  const response = {
+    statusCode: 200,
+    body: `The random value of ${max} is : ${val} `,
+  };
+  return response;
+};
