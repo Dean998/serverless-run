89c0e66a0b0128f60ce126c637b631bcfee00d55
Extra <extra@deans-mbp.home>
2023-02-28 23:34:24 +0000

    added open ai api into the codebase

--- src/handler.ts
+++ src/handler.ts
@@ -1,5 +1,6 @@
-import axios from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
+require("dotenv").config();
 
 export const hello = async (
   event: APIGatewayProxyEvent
@@ -10,7 +11,7 @@ export const hello = async (
   };
 };
 
-export const openai = async (
+export const random = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
   const response = await axios.get("https://randomuser.me/api/");
@@ -18,4 +19,114 @@ export const openai = async (
     statusCode: 200,
     body: JSON.stringify(response.data),
   };
+};
+
+export const openAi = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  try {
+    const config: AxiosRequestConfig = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+    };
+
+    const data = {
+      model: "text-davinci-003",
+      prompt: "Say this is a test",
+      temperature: 0,
+      max_tokens: 7,
+    };
+    const response = await axios.post(
+      "https://api.openai.com/v1/completions",
+      data,
+      config
+    );
+    console.log(response.data?.choices);
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(response.data),
+    };
+  } catch (error) {
+    console.log(error.message);
+    console.log(error.response.data);
+  }
+};
+export const openAiImageGenerator = async (
+  event: APIGatewayProxyEvent
+): Promise<APIGatewayProxyResult> => {
+  try {
+    const config: AxiosRequestConfig = {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+      },
+    };
+
+    const data = {
+      prompt: "A potato flying in the air with wings",
+      n: 1,
+      size: "1024x1024",
+    };
+    const response = await axios.post(
+      "https://api.openai.com/v1/images/generations",
+      data,
+      config
+    );
+
+    const imageUrl = response.data.data[0].url;
+
+    const html = `
+      <!DOCTYPE html>
+      <html>
+        <head>
+          <meta charset="utf-8">
+          <title>Loading...</title>
+          <style>
+            body {
+              background-color: #f1f1f1;
+              font-family: Arial, sans-serif;
+            }
+            .loading-message {
+              text-align: center;
+              margin-top: 50px;
+            }
+            .redirect-message {
+              text-align: center;
+              margin-top: 50px;
+              display: none;
+            }
+          </style>
+        </head>
+        <body>
+          <div class="loading-message">
+            <h1>Your image is loading...</h1>
+          </div>
+          <div class="redirect-message">
+            <h1>Redirecting to your image...</h1>
+          </div>
+          <script>
+            setTimeout(function() {
+              document.querySelector('.loading-message').style.display = 'none';
+              document.querySelector('.redirect-message').style.display = 'block';
+              window.location.href = '${imageUrl}';
+            }, 5000); // Set a 5-second delay before redirecting
+          </script>
+        </body>
+      </html>
+    `;
+
+    return {
+      statusCode: 200,
+      headers: {
+        "Content-Type": "text/html",
+      },
+      body: html,
+    };
+  } catch (error) {
+    console.log(error.message);
+    console.log(error.response.data);
+  }
 };
